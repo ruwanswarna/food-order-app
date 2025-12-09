f58dd33fb1b4eb1c07010e1521c374668501f195
ruwanswarna <ruwanswarna@gmail.com>
2025-12-09 09:34:38 +0530

    header and cart button added

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+	return (
+		<header id="main-header">
+			<div id="title">
+				<img src="logo.jpg" alt="logo image" />
+				<h1>Food Lover Takeaway</h1>
+			</div>
+			<nav>
+				<button>Cart (0)</button>
+			</nav>
+		</header>
+	);
+}
