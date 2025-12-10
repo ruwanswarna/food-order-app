ed749245c67c0a92bf7d6828d87baf292b0316a0
ruwanswarna <ruwanswarna@gmail.com>
2025-12-10 19:23:15 +0530

    add and remove items from cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,6 +3,7 @@ import Modal from "./UI/Modal";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 export default function Cart() {
 	const cartCtx = useContext(CartContext);
 	const userProgressCtx = useContext(UserProgressContext);
@@ -18,9 +19,12 @@ export default function Cart() {
 			<h2>Your Cart</h2>
 			<ul>
 				{cartCtx.items.map((item) => (
-					<li key={item.id}>
-						{item.name} - {item.quantity}
-					</li>
+					<CartItem
+						key={item.id}
+						item={item}
+						onDecrease={()=>cartCtx.removeItem(item.id)}
+						onIncrease={()=>cartCtx.addItem(item)}
+					/>
 				))}
 			</ul>
 			<p className="cart-total">${cartTotal}</p>
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,14 @@
+export default function CartItem({ item, onIncrease, onDecrease }) {
+	return (
+		<li key={item.id} className="cart-item">
+			<p>
+				{item.name} - {item.quantity} x ${item.price}
+			</p>
+			<p className="cart-item-actions">
+				<button onClick={onDecrease}>-</button>
+				<span>{item.quantity}</span>
+				<button onClick={onIncrease}>+</button>
+			</p>
+		</li>
+	);
+}
