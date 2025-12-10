c6310334abf293219021498a8f282bf66bb910bb
ruwanswarna <ruwanswarna@gmail.com>
2025-12-10 18:08:23 +0530

    fixed an error

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,11 +5,14 @@ import CartContext from "../store/CartContext";
 import UserProgressContext from "../store/UserProgressContext";
 export default function Cart() {
 	const cartCtx = useContext(CartContext);
-    const userProgressCtx = useContext(UserProgressContext);
+	const userProgressCtx = useContext(UserProgressContext);
 	const cartTotal = cartCtx.items.reduce(
 		(totalPrice, item) => totalPrice + item.quantity * item.price,
 		0
 	);
+	function handleCloseCart() {
+		userProgressCtx.hideCart();
+	}
 	return (
 		<Modal className="cart">
 			<h2>Your Cart</h2>
@@ -20,11 +23,15 @@ export default function Cart() {
 					</li>
 				))}
 			</ul>
-			<p className="cart-total" open = {userProgressCtx.progress==='cart'}>${cartTotal}</p>
-            <p className="modal-actions">
-                <Button textOnly>Close</Button>
-                <Button>Go to checkout</Button>
-            </p>
+			<p className="cart-total" open={userProgressCtx.progress === "cart"}>
+				${cartTotal}
+			</p>
+			<p className="modal-actions">
+				<Button textOnly onClick={handleCloseCart}>
+					Close
+				</Button>
+				<Button onClick={handleCloseCart}>Go to checkout</Button>
+			</p>
 		</Modal>
 	);
 }
